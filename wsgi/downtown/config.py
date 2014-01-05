e74911b59d02f6762ff9e929c6b8bacfd290f01c
Sam <mirthquakes@gmail.com>
2014-01-05 14:02:57 -0600

    add basic app files

---
+++ wsgi/downtown/config.py
@@ -0,0 +1,13 @@
+"""Application configuration.
+
+When using app.config.from_object(obj), Flask will look for all UPPERCASE
+attributes on that object and load their values into the app config. Python
+modules are objects, so you can use a .py file as your configuration.
+"""
+
+import os
+
+# Get the current working directory to place sched.db during development.
+# In production, use absolute paths or a database management system.
+PWD = os.path.abspath(os.curdir)
+DEBUG = True
