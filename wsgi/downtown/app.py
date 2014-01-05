f1abbb1c937714301fa58f59a40c3f425cfea06f
Sam <mirthquakes@gmail.com>
2014-01-05 14:17:57 -0600

    configured application file

---
+++ wsgi/downtown/app.py
@@ -0,0 +1,12 @@
+import logging
+
+from flask import Flask
+from flask import abort, jsonify, redirect, render_template, request, url_for , flash
+from flask.ext.login import LoginManager, current_user
+from flask.ext.login import login_user, login_required, logout_user
+from flask.ext.sqlalchemy import SQLAlchemy
+
+import config
+
+app = Flask(__name__)
+app.config.from_object(config)
