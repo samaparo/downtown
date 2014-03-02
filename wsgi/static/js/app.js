69bce833d3a03a85fbe43662e9c1992c9d0f74f1
Sam <mirthquakes@gmail.com>
2014-03-02 17:04:49 -0600

    add in cheeky paging button

--- wsgi/static/js/app.js
+++ wsgi/static/js/app.js
@@ -8,7 +8,42 @@ var currentPage = 0;
 	imageTemplate = $("#template_image").html();
 	
 	$("#buttonWrapper button").click(function () {
-		console.log("click");
+		currentPage += 1;
+		$.ajax({
+			url: "/api/images/page/" + currentPage,
+			success: function (data) {
+				var images = data.IMAGES;
+				var imageHTML = "";
+				if (images.length > 0) {
+					for (var i = 0; i < images.length; i++) {
+						imageHTML += Mustache.render(imageTemplate, images[i]);
+					}
+				}
+				else{
+					$("#buttonWrapper button").hide();
+				}
+				$("#imageContainer").append(imageHTML);
+				
+			}
+		});
+		
+		var buttonText;
+		switch (currentPage % 4) {
+		case 0:
+			buttonText = "Show me more!";
+			break;
+		case 1:
+			buttonText = "Keep 'em coming!";
+			break;
+		case 2:
+			buttonText = "I'd like s'more.";
+			break;
+		case 3:
+			buttonText = "More more more...";
+			break;
+		}
+		$("#buttonWrapper button").text(buttonText);
+		
 	});
 	
 }());
